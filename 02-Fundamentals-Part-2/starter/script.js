1daf7bbdaa8e5d54fe6a3725d85b9a44806f81dd
Aleajndro <marcaledom26@gmail.com>
2023-10-28 20:24:12 -0300

    CODING EXERCICSES

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -254,8 +254,56 @@
 //     console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
 // }
 
-let rep = 1;
-while (rep <= 10) {
-    // console.log(`WHILE: Lifting weights repetition ${rep} 🏋️‍♀️`);
-    rep++;
-}
+// let rep = 1;
+// while (rep <= 10) {
+//     // console.log(`WHILE: Lifting weights repetition ${rep} 🏋️‍♀️`);
+//     rep++;
+// }
+
+// let dice = Math.trunc(Math.random() * 4) + 1;
+// console.log(dice);
+
+// while (dic !== 6) {
+//     console.log(`You rolled ${dice}`)
+//     dice = Math.trunc(Math.random() * 4) + 1;
+
+// }
+
+/// CODING EXERCISES
+// const calcTip = function (bill) {
+//     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+//   }
+  
+//   /* Write your code below. Good luck! 🙂 */
+  
+//   const bills = [ 22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+//   const tips = [];
+//   const totals = [];
+  
+//   for (let i = 0; i < bills.length; i++) {
+//       const tip = calcTip(bills[i]);
+//       tips.push(tip);
+//       totals.push(tip + bills[i]);
+//   }
+  
+//   console.log(bills);
+//   console.log(tips);
+//   console.log(totals);
+  
+  
+//   const calcAverage = function (arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//        sum += arr[i];
+//   console.log(`Iteración ${i + 1}: arr[i] = ${arr[i]}, sum = ${sum}`);
+//     }
+//     return sum / arr.length;
+//   };
+//   console.log(calcAverage([10 , 20 ,100]));
+//   console.log(calcAverage(totals));
+//   console.log(calcAverage(tips));
+  
+  
+  
+  
+  
